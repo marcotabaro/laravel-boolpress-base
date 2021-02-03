ce873bfd3946116abc929de9888bdf787812df1b
marco <g.marcotaba@gmail.com>
2021-02-03 18:03:14 +0100

    modified category factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,9 +4,13 @@
 
 use App\Category;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $title = $faker->realText(50, 1);
+    $slug = Str::of($title)->slug('-');
     return [
-        //
+        'title' => $title,
+        'slug' => $slug,
     ];
 });
