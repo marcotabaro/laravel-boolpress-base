13edddeeca682a48ffce6ee7e7ef604ecfe991e1
marco <g.marcotaba@gmail.com>
2021-02-03 23:01:15 +0100

    bug fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             PostSeeder::class,
-            PostInformation::class
+            PostInformationSeeder::class,
             ]);
     }
 }
